b7b2f4320789ed6c16655b3601aa624cc6c4578d
Giovanni <giovannimasi03@gmail.com>
2025-06-23 12:52:03 +0200

    PI

--- Examples/R2024b/c2b/DualMotorDynoUsingC2000ProcessorsExample(old)/c28377Spmsmfocdual_ert.m
+++
@@ -1,5 +0,0 @@
-% Script to direct users to f28379d dyno motor control example
-
-% Copyright 2020-2021 The MathWorks, Inc.
-
-warning(message('TIC2000:demos:tic2000MotorControlDynoExample','c28377Spmsmfocdual_ert'));
